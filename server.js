d4b039088a7e5b09db6a2f93bc7378262b8cc4bb
abhinavnist <abhinavpatna55@gmail.com>
2024-10-20 20:27:29 +0530

    jfld

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/auth", authRoutes)
+app.get("/", (req, res) => {
+  res.json({ message: "aastha-telehealth-backend-server" })
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
